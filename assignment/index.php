388b352cb559b801b07f89c1fb058accc94c7746
fy73 <110988544+UuU210@users.noreply.github.com>
2024-05-16 10:25:51 +0100

    Add files via upload

---
+++ assignment/index.php
@@ -0,0 +1,12 @@
+<?php
+// Load the core application
+require_once 'app/model/model.php';
+require_once 'app/controller/controller.php';
+
+
+$controller = new Controller();
+
+
+$controller->load();
+
+?>
